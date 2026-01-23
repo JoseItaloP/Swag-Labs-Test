0960a30a0e4336fcfabc60dcf261ad3c55fe3c44
Jose Italo <122283414+JoseItaloP@users.noreply.github.com>
2026-01-23 16:53:08 -0300

    test 7

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,7 +9,7 @@ describe('Testing the login', ()=>{
     })
 
     it('Sucefully pass test in standard_user', () => { 
-
+        cy.log(Cypress.env('PASS'))
         login.makeLogin(Cypress.env('STANDART_USER'), Cypress.env('PASS'))
 
         Inventory.confirmatingPage()
